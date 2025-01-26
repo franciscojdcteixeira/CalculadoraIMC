c55adf2f6467f6ae571c8d165a1e4d2b29ca53e4
franciscojdcteixeira <franciscojdcteixeira@gmail.com>
2025-01-26 01:00:24 +0000

    mensagem a pedir para preencher campos vazios

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //Recuperar os componentes EditText
-            // Criar uma variável e associar (=) o componente de UI <TextInputEditText>
+        // Criar uma variável e associar (=) o componente de UI <TextInputEditText>
         //Recuperar o botão
-            //Colocar acção no botão
+        //Colocar acção no botão
         //Recuperar o texto colocado no edt peso
 
         val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
@@ -22,16 +23,33 @@ class MainActivity : AppCompatActivity() {
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
         btnCalcular.setOnClickListener {
-            val peso: Float = edtPeso.text.toString().toFloat()
-            val altura: Float = edtAltura.text.toString().toFloat()
-
-            val alturaQ2 = altura * altura
-            val resultado = peso / alturaQ2
-
-            println( "Teste! " + resultado)
 
 
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
 
+            if (pesoStr == "" || alturaStr == "") {
+                //Mostrar mensagem ao utilizador
+                Snackbar.make(
+                    edtPeso,
+                    "Preenche todos os campos",
+                    Snackbar.LENGTH_LONG
+                )
+                    .show()
+
+
+            } else {
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
+
+
+                val alturaQ2 = altura * altura
+                val resultado = peso / alturaQ2
+
+                println("Teste! " + resultado)
+
+
+            }
 
         }
 
